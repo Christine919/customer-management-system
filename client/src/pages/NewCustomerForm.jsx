a217e9e7308ddefe86b8f176df44af4df0434984
Christine <161996117+Christine919@users.noreply.github.com>
2024-10-16 11:12:57 +0800

    Remove email required

--- client/src/pages/NewCustomerForm.jsx
+++ client/src/pages/NewCustomerForm.jsx
@@ -195,7 +195,6 @@ const NewCustomerForm = () => {
             value={formData.email}
             onChange={handleChange}
             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-            required
           />
         </div>
         <div className="mb-4">
