59cdef3790d5b06cfb45de7dcf58e2f9f9d917ac
Christine <161996117+Christine919@users.noreply.github.com>
2024-10-16 11:11:04 +0800

    Fix hamburget menu responsive on ipad screen

--- client/src/pages/dashboards/DashboardLayout.jsx
+++ client/src/pages/dashboards/DashboardLayout.jsx
@@ -20,17 +20,17 @@ const DashboardLayout = () => {
   return (
     <div className="flex h-screen">
       {/* Sidebar */}
-      <div className={`bg-gradient-to-b from-[#fdf5e6] to-[#f8e1dd] ${sidebarOpen ? 'block' : 'hidden'} md:block fixed h-full z-20 w-64 md:w-64 lg:w-64 xl:w-64 2xl:w-64`}>
-        <button className="absolute top-4 right-4 p-2 md:hidden" onClick={toggleSidebar}>
+      <div className={`bg-gradient-to-b from-[#fdf5e6] to-[#f8e1dd] ${sidebarOpen ? 'block' : 'hidden'} lg:block fixed h-full z-20 w-64 lg:w-64 xl:w-64 2xl:w-64`}>
+        <button className="absolute top-4 right-4 p-2 lg:hidden" onClick={toggleSidebar}>
           <XIcon className="h-6 w-6 text-gray-500" />
         </button>
         <Sidebar setSidebarOpen={setSidebarOpen}/>
       </div>
 
       {/* Main Content */}
-      <div className="w-full flex flex-col md:ml-64">
+      <div className="w-full flex flex-col lg:ml-64">
         {/* Hamburger Menu Button for Mobile */}
-        <button className="md:hidden p-4 bg-gradient-to-b from-[#fdf5e6] to-[#f8e1dd]" onClick={toggleSidebar}>
+        <button className="lg:hidden p-4 bg-gradient-to-b from-[#fdf5e6] to-[#f8e1dd]" onClick={toggleSidebar}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
           </svg>
